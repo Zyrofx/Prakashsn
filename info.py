27afaa8a8ce30f4f267192d528d6ee5aba3943d1
Zyrofx <144825148+Zyrofx@users.noreply.github.com>
2025-05-05 22:04:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['23789353'])
-API_HASH = environ['fcc7f1c8b86d3e2062218a24b617e23d']
-BOT_TOKEN = environ['6156063299:AAG5B1x8H-KaLW3plhVADRGWMG-infVlZko']
+API_ID = int(environ.get('API_ID', '23789353')) 
+API_HASH = environ.get('API_HASH', 'fcc7f1c8b86d3e2062218a24b617e23d')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6156063299:AAG5B1x8H-KaLW3plhVADRGWMG-infVlZko')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
